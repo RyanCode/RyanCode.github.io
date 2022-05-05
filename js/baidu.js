343c557bc679fd8f0cf62f6de666f431ef10d718
ryan <ryanwymail@163.com>
2022-05-05 21:00:57 +0800

    deploy

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+module.exports = {
+	var _hmt = _hmt || [];
+	(function() {
+	  var hm = document.createElement("script");
+	  hm.src = "https://hm.baidu.com/hm.js?3ab3d4cf25cd3fd346f82c4395a685c6";
+	  var s = document.getElementsByTagName("script")[0]; 
+	  s.parentNode.insertBefore(hm, s);
+	})();
+}
